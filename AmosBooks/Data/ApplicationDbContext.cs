a316860104d8c426815e4ef85db0b676a1866f0d
zinvolle <134840732+zinvolle@users.noreply.github.com>
2024-10-21 21:54:49 +1100

    Added category model and controller

---
+++ AmosBooks/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AmosBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmosBooks.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
