2271f4204d9e729f098486783d11ac9013f467dd
zinvolle <aeroaviation12@gmail.com>
2024-11-15 20:54:55 +1100

    Added Product Views

--- AmosBooks/Areas/Admin/Controllers/ProductController.cs
+++ AmosBooks/Areas/Admin/Controllers/ProductController.cs
@@ -16,14 +16,26 @@ namespace AmosBooks.Areas.Admin.Controllers
         public IActionResult Index()
         {
             var objProductList = _unitOfWork.Product.GetAll().ToList();
-            return View();
+            return View(objProductList);
         }
 
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["Success"] = "Product added successfully";
+                return RedirectToAction("Index");
+            }
 
+            return View();
+        }
         public IActionResult Edit(int? id)
         {
             if(id == null || id == 0)
@@ -55,7 +67,19 @@ namespace AmosBooks.Areas.Admin.Controllers
 
         public IActionResult Delete(int id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(productFromDb);
         }
     }
 }
